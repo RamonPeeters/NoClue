6d6f57d473a808b21f7522bd218cf634aa6ee762
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-10-13 11:38:14 +0200

    Pass a delegate in the GameRoomCollection constructor to provide the room code

--- NoClue.Core/Rooms/GameRoomCollection.cs
+++ NoClue.Core/Rooms/GameRoomCollection.cs
@@ -4,35 +4,34 @@ using System.Collections.Generic;
 namespace NoClue.Core.Rooms {
     public class GameRoomCollection {
         private const int MAX_ATTEMPTS = 1000;
-        private static readonly Random ROOM_CODE_RNG = new Random();
 
-        private readonly int MaxRoomCount;
+        private readonly Func<int> RoomCodeProvider;
         private readonly Dictionary<int, GameRoom> Rooms = new Dictionary<int, GameRoom>();
 
-        public GameRoomCollection(int maxRoomCount) {
-            if (maxRoomCount < 0) {
-                throw new ArgumentOutOfRangeException(nameof(maxRoomCount));
-            }
-            MaxRoomCount = maxRoomCount;
+
+        public GameRoomCollection(Func<int> roomCodeProvider) {
+            RoomCodeProvider = roomCodeProvider;
         }
 
         public bool TryCreate(out int code) {
-            code = GetUniqueCode();
-            if (code == -1) {
+            int? uniqueCode = GetUniqueCode();
+            if (!uniqueCode.HasValue) {
+                code = 0;
                 return false;
             }
+            code = uniqueCode.Value;
             Rooms.Add(code, new GameRoom());
             return true;
         }
 
-        private int GetUniqueCode() {
+        private int? GetUniqueCode() {
             for (int attempts = 0; attempts < MAX_ATTEMPTS; attempts++) {
-                int code = ROOM_CODE_RNG.Next(MaxRoomCount);
+                int code = RoomCodeProvider();
                 if (!Rooms.ContainsKey(code)) {
                     return code;
                 }
             }
-            return -1;
+            return null;
         }
     }
 }
--- NoClue.Tests/Rooms/GameRoomCollectionTests.cs
+++ NoClue.Tests/Rooms/GameRoomCollectionTests.cs
@@ -1,28 +1,26 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NoClue.Core.Rooms;
-using System;
 
 namespace NoClue.Tests.Rooms {
     [TestClass]
     public class GameRoomCollectionTests {
         [TestMethod]
-        public void Constructor_ThrowsException_BecauseMaxRoomCountIsNegative() {
-            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new GameRoomCollection(-10));
-        }
-
-        [TestMethod]
         public void TryCreate_ReturnsTrue() {
-            GameRoomCollection rooms = new GameRoomCollection(10);
+            GameRoomCollection rooms = new GameRoomCollection(CreateRoomCode);
             bool successful = rooms.TryCreate(out _);
             Assert.IsTrue(successful);
         }
 
         [TestMethod]
         public void TryCreate_ReturnsFalse_BecauseThereAreTooManyRooms() {
-            GameRoomCollection rooms = new GameRoomCollection(1);
+            GameRoomCollection rooms = new GameRoomCollection(CreateRoomCode);
             rooms.TryCreate(out _);
             bool successful = rooms.TryCreate(out _);
             Assert.IsFalse(successful);
+        }
+
+        private static int CreateRoomCode() {
+            return 1;
         }
     }
 }
